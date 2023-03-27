bd8c4712c61213fd32beef790c84ccbc87d0b3ed
ex.neskoro <ex.neskoro@gmail.com>
2023-03-27 16:28:46 +0500

    Added Commutator tests

--- src/test/java/RotorTests.java
+++ src/test/java/RotorTests.java
@@ -1,15 +1,21 @@
+import ex.neskoro.Commutator;
 import ex.neskoro.language.EnLanguage;
 import ex.neskoro.language.Language;
 import ex.neskoro.language.RuLanguage;
+import ex.neskoro.rotor.EntryRotor;
 import ex.neskoro.rotor.Rotor;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class RotorTests {
     Rotor rotor;
+    Commutator commutator;
+    EntryRotor entryRotor;
 
     private final Language EN = new EnLanguage();
     private final Language RUS = new RuLanguage();
@@ -19,18 +25,20 @@ public class RotorTests {
     private final String LETTER_A = "a";
     private final String LETTER_GA = "GA";
 
-    private final String DEF_STATE = "0,xlhibzwjnrseqaftvkodpmycug,a";
-    private final String DEF_STATE_3 = "0,xlhibzwjnrseqaftvkodpmycug,azt";
-    private final String DEF_STATE_15 = "0,ilntoyrajspmfbwzcxhdqugvke,awertyuioxcvbnm";
+    private final String DEF_ROTOR_STATE = "0,xlhibzwjnrseqaftvkodpmycug,a";
+    private final String DEF_ROTOR_STATE_3 = "0,xlhibzwjnrseqaftvkodpmycug,azt";
+    private final String DEF_ROTOR_STATE_15 = "0,ilntoyrajspmfbwzcxhdqugvke,awertyuioxcvbnm";
 
     @BeforeEach
     void setRotor() {
         rotor = new Rotor(DEFAULT_LAN);
+        commutator = new Commutator(DEFAULT_LAN);
+        entryRotor = new EntryRotor(DEFAULT_LAN);
     }
 
 
     @Test
-    void importExportState() {
+    void rotorImportExportState() {
         String initState = rotor.exportState();
 
         rotor = new Rotor(DEFAULT_LAN);
@@ -43,8 +51,8 @@ public class RotorTests {
     }
 
     @Test
-    void sameStateSameOutputDefault() {
-        rotor.importState(DEF_STATE);
+    void rotorSameStateSameOutputDefault() {
+        rotor.importState(DEF_ROTOR_STATE);
 
         String output = rotor.processLetterIn(LETTER_A, 0);
 
@@ -53,10 +61,58 @@ public class RotorTests {
 
     @Test
     void turnRotor() {
-        rotor.importState(DEF_STATE);
+        rotor.importState(DEF_ROTOR_STATE);
 
         boolean turnNext = rotor.turn();
 
         assertFalse(turnNext);
+    }
+
+    @Test
+    void commutatorImportExportState() {
+        String initState = commutator.exportState();
+
+        commutator = new Commutator(DEFAULT_LAN);
+
+        commutator.importState(initState);
+        String exportedState = commutator.exportState();
+
+        assertEquals(initState, exportedState);
+    }
+
+    @Test
+    void commutatorRandomMapSizeIsEven() {
+        List<Boolean> expectedList = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            expectedList.add(true);
+        }
+
+        List<Boolean> resultList = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            resultList.add(commutator.exportState().length() % 2 == 0);
+            commutator = new Commutator(DEFAULT_LAN);
+        }
+
+        assertEquals(expectedList, resultList);
+    }
+
+    @Test
+    void commutatorPairLetters() {
+        commutator.importState(commutator.exportState());
+
+        String[] letters = commutator.exportState().split("");
+
+        List<Boolean> expectedList = new ArrayList<>();
+        for (int i = 0; i < letters.length; i++) {
+            expectedList.add(true);
+        }
+
+        List<Boolean> resultList = new ArrayList<>();
+        for (int i = 0; i < letters.length; i += 2) {
+            resultList.add(commutator.processLetter(letters[i]).equals(letters[i+1]));
+            resultList.add(commutator.processLetter(letters[i+1]).equals(letters[i]));
+        }
+
+        assertEquals(expectedList, resultList);
     }
 }
